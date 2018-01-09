166ec524bbe88e6ebddb465485d83582f265d221
Evan <cpp971019@gmail.com>
2018-01-09 16:24:43 +0800

    Init

---
+++ app/src/main/java/com/androidproject/comarkdown/data/DownloadInfo.kt
@@ -0,0 +1,10 @@
+package com.androidproject.comarkdown.data
+
+import java.io.File
+
+/**
+ * Created by evan on 2018/1/9.
+ */
+data class DownloadInfo(
+        var file:File
+)
--- app/src/main/java/com/androidproject/comarkdown/network/ApiServer.kt
+++ app/src/main/java/com/androidproject/comarkdown/network/ApiServer.kt
@@ -1,9 +1,12 @@
 package com.androidproject.comarkdown.network
 
+import com.androidproject.comarkdown.data.DownloadInfo
 import com.androidproject.comarkdown.data.LoginInfo
 import com.androidproject.comarkdown.data.RegisterInfo
+import com.androidproject.comarkdown.data.UploadInfo
 import io.reactivex.Observable
 import retrofit2.http.*
+import java.io.File
 
 /**
  * Created by evan on 2017/12/31.
@@ -19,4 +22,17 @@ interface ApiServer {
     fun register(@Field("username") username: String,
                  @Field("password") password: String,
                  @Field("email") email: String): Observable<RegisterInfo>
+
+    @FormUrlEncoded
+    @POST("upload_file/")
+    fun uploadFile(@Field("username") username: String,
+                   @Field("token") token: String,
+                   @Field("filename") filename: String,
+                   @Field("file") file:File): Observable<UploadInfo>
+
+    @FormUrlEncoded
+    @POST("download_file/")
+    fun downloadFile(@Field("username") username: String,
+                   @Field("token") token: String,
+                   @Field("filename") filename: String): Observable<DownloadInfo>
 }
